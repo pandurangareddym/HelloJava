80f116fb5a77bdd36e59017dc9754d2ee8c6e1b6
panduranga reddy <panduranga@oc7488252688.ibm.com>
2019-07-16 15:31:33 +0530

    third commit

--- HelloJava.java
+++ HelloJava.java
@@ -2,5 +2,6 @@ public class HelloJava {
 	public static void main (String args[]) {
 		System.out.println("Hello Java");
 		System.out.println("The code is pushing from GitHub");
+		System.out.println("The build is automatic at 10 minitus");
 	}
 }
